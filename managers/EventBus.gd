2d4f9adcd70abb57f612355a9dd6fec695a02462
xiaozhimk <2430822605@qq.com>
2025-11-25 14:13:17 +0800

    Update player_mana_changed signal parameters

--- managers/EventBus.gd
+++ managers/EventBus.gd
@@ -10,4 +10,4 @@ signal player_died
 
 signal enemy_defeated(enemy_type: String)
 
-signal player_mana_changed(current_mana: int, max_mana: int)
+signal player_mana_changed(normal_mana: int, max_normal_mana: int, condensed_mana: int, max_condensed_mana: int)
